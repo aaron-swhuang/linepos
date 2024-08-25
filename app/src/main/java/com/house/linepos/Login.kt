afd449357a3b1e1adea55fa9ec5ae8c4e587937f
ShaoWei Huang <aaron0327@gmail.com>
2024-08-25 12:24:44 +0800

    Add keyboard options for username and password field
    Add keyboard options for username and password field.
    Also, set imeAction as Next for username field and set Done for
    password field.

--- app/src/main/java/com/house/linepos/Login.kt
+++ app/src/main/java/com/house/linepos/Login.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -18,6 +19,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,6 +41,10 @@ fun LoginPage() {
                 value = username,
                 label = { Text(stringResource(R.string.user_name)) },
                 maxLines = 1,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Next
+                ),
                 onValueChange = { username = it },
                 modifier = Modifier.padding(8.dp)
             )
@@ -46,6 +53,10 @@ fun LoginPage() {
                 value = password,
                 label = { Text(stringResource(R.string.password))},
                 visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                ),
                 onValueChange = { password = it},
                 modifier = Modifier.padding(8.dp)
             )

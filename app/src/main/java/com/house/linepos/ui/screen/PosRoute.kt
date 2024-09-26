d07ec81f2846a0cdf4f5e44d8da5d10f9e1faf0d
Aaron Huang <aaron0327@gmail.com>
2024-09-26 22:37:43 +0800

    Rename route class to PosRoute
    This patch only rename class name.

--- app/src/main/java/com/house/linepos/ui/screen/PosRoute.kt
+++ app/src/main/java/com/house/linepos/ui/screen/PosRoute.kt
@@ -21,67 +21,68 @@ interface BaseLinePosRoute {
     val description: String?
 }
 
-interface LinePosRoute : BaseLinePosRoute {
+interface PosRoute : BaseLinePosRoute {
     val icon: ImageVector
 }
 
-object Main : LinePosRoute {
+object Main : PosRoute {
     override val icon = Icons.AutoMirrored.Outlined.ExitToApp
     override val route = "main"
     override val label = "Main"
     override val description = "main screen"
 }
 
-object LoginPage : LinePosRoute {
+object LoginPage : PosRoute {
     override val icon = Icons.AutoMirrored.Outlined.ExitToApp
     override val route = "login"
     override val label = "Login"
     override val description = "login screen"
 }
 
-object Logout : LinePosRoute {
+object Logout : PosRoute {
     override val icon = Icons.AutoMirrored.Outlined.ExitToApp
     override val route = "logout"
     override val label = "Logout"
     override val description = "Direct to login screen"
 }
 
-object Notification : LinePosRoute {
+object Notification : PosRoute {
     override val icon = Icons.Outlined.Notifications
     override val route = "notification"
     override val label = "Notification"
     override val description = "Direct to notification screen"
 }
 
-object ShoppingCart : LinePosRoute {
+object ShoppingCart : PosRoute {
     override val icon = Icons.Outlined.ShoppingCart
     override val route = "shopping_cart"
     override val label = "Shopping Cart"
     override val description = "shopping cart"
 }
 
-object Home : LinePosRoute {
+// Items of bottom bar
+object Home : PosRoute {
     override val icon = Icons.Outlined.Home
     override val route = "home"
     override val label = "Home"
     override val description = "home screen"
 }
 
-object NewOrder : LinePosRoute {
+object NewOrder : PosRoute {
     override val icon = Icons.Outlined.Add
     override val route = "new_order"
     override val label = "New Order"
     override val description = "create a new order"
 }
 
-object Info : LinePosRoute {
+object Info : PosRoute {
     override val icon = Icons.AutoMirrored.Outlined.List
     override val route = "info"
     override val label = "Info"
     override val description = "Info"
 }
 
-object About : LinePosRoute {
+object About : PosRoute {
     override val icon = Icons.Outlined.Info
     override val route = "about"
     override val label = "About"
@@ -115,7 +116,7 @@ object ProductTag : BaseLinePosRoute {
 // TODO: Currently, this is used by dropdown menu and it does not navigate to
 //       another screen so this should be considered redefine for dropdown menu
 //       or set the icon and description inline.
-object Settings : LinePosRoute {
+object Settings : PosRoute {
     override val icon = Icons.Outlined.Settings
     override val route = "settings"
     override val label = "Settings"
